8e90c5d41c73264a9b4b23dafc920fc4f25d20eb
Andrei Ivascu <andrew.ivascu@gmail.com>
2015-08-07 17:41:51 +0300

    Unit test implementation attempt

--- src/Wix.AssemblyInfo.Tests/WixAssemblyInfoPreprocessorExtensionTests.cs
+++ src/Wix.AssemblyInfo.Tests/WixAssemblyInfoPreprocessorExtensionTests.cs
@@ -5,5 +5,24 @@ namespace Wix.AssemblyInfo.Tests
     [TestFixture]
     public class WixAssemblyInfoPreprocessorExtensionTests
     {
+        [Test]
+        [TestCase("", "", "")]
+        [TestCase(null, null, null)]
+        [TestCase("Wrong", "Wrong", "Wrong")]
+        [TestCase("", "AssemblyTitle", @".\TestAssembly.dll")]
+        [TestCase(null, "AssemblyTitle", @".\TestAssembly.dll")]
+        [TestCase("   ", "AssemblyTitle", @".\TestAssembly.dll")]
+        [TestCase("myWrongPrefix", "AssemblyTitle", @".\TestAssembly.dll")]
+        public void TestPrefix(string prefix, string function, string assemblyPath)
+        {
+            var preprocessorExtension = new WixAssemblyInfoPreprocessorExtension();
+            string[] args = {
+                assemblyPath
+            };
+
+            var result = preprocessorExtension.EvaluateFunction(prefix, function, args);
+
+            Assert.IsNull(result, "The prefix somehow got processed");
+        }
     }
 }
